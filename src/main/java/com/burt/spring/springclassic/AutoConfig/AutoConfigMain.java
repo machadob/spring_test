33778bdc36d8fc34b73e2d1e8f4741a2bc538a3c
burton.machado@gmail.com <burton.machado@gmail.com>
2020-02-02 11:53:40 -0500

    Ongoing Updates

--- src/main/java/com/burt/spring/springclassic/AutoConfig/AutoConfigMain.java
+++ src/main/java/com/burt/spring/springclassic/AutoConfig/AutoConfigMain.java
@@ -3,7 +3,7 @@ package com.burt.spring.springclassic.AutoConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class AutoConfig {
+public class AutoConfigMain {
     public static void main(String[] args) {
         @SuppressWarnings("resource")
         ApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
